48a6ecd1c4aebf86acb728a9b1e74e9e61fc7128
Nick Desktop <nicknelson1021@gmail.com>
2022-06-21 11:49:20 -0500

    remove index from csv

--- streamlit_app.py
+++ streamlit_app.py
@@ -314,7 +314,7 @@ if st.session_state.create_data:
     st.markdown("### Data preview")
     st.dataframe(df_grouped)
 
-    csv = df.to_csv().encode('utf-8')
+    csv = df.to_csv(index=False).encode('utf-8')
     st.download_button("Press to Download", csv, "a_b_test_data.csv", "text/csv", key='download-csv')
 
     st.markdown("### Select columns for analysis")
